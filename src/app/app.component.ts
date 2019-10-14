b218bc17a300874ab7599e4ec29fdb84e55b5e1a
akagrawat <akagrawat@gmail.com>
2019-10-14 17:49:11 +0530

    simple drag and drop

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Drag Drop';
+  numbers: number[]= [];
+  otherNumbers: number[] = [];
+  constructor(){
+for (let i = 0;i< 10; i++){
+  this.numbers.push(i);
+}
+  }
+
+drop(event:CdkDragDrop<number[]>){
+  if(event.previousContainer !== event.container){
+ transferArrayItem(event.previousContainer.data, event.container.data,event.previousIndex,event.currentIndex);
+  }
+  else{
+  moveItemInArray(this.numbers,event.previousIndex, event.currentIndex) 
+}
+}
+}
+
